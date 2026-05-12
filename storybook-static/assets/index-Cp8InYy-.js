48df7add1b0346ab582a34983649aa11039cbf69
arodriguez <arodriguez@ecna.es>
2026-05-12 16:17:23 +0200

    cambios v2

---
+++ storybook-static/assets/index-Cp8InYy-.js
@@ -0,0 +1,1 @@
+import{M as a,u as n}from"./index-BuvqkFXL.js";import"./index-B87U9ST6.js";import"./iframe-DZaS-gSx.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-BHYIh-Xd.js";import"./index-DrFu-skq.js";export{a as MDXProvider,n as useMDXComponents};
